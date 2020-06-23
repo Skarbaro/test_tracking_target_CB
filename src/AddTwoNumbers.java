bb6c5cb8d86dbdc518ad42136222710992756dfb
Alex Romaniuk <alaster.rom@gmail.com>
2020-06-23 12:04:25 +0300

    ad num / dif

--- src/AddTwoNumbers.java
+++ src/AddTwoNumbers.java
@@ -4,8 +4,7 @@ public class AddTwoNumbers {
    
    public static void main(String[] args) {
       
-      int num1 = 5, num2 = 15;
-	  int sum;
+      int num1 = 5, num2 = 15, sum;
 
       sum = num1 + num2;
       
