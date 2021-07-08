0caa5c64552e839880171f8973e88e4eddbfc82a
Alex Romaniuk <alaster.rom@gmail.com>
2021-07-08 20:57:34 +0300

    no message

--- src/AddTwoNumbers.java
+++ src/AddTwoNumbers.java
@@ -2,14 +2,20 @@ package src;
 
 public class AddTwoNumbers {
    
-   public static void main(String[] args) {
+   public static void main(String args[]) {
       
-      int num1 = 5, num2 = 15;
-	  int sum;
+      int num1 = 5, num2 = 15, sum;
 
       sum = num1 + num2;
       
       logger.log("Sum of these numbers: " + sum);
    }
 
+
+	public static void main(String args[]) {
+	   
+	  private int red1 = 5, red2 = 10, rif;
+      rif = red1 - red2;
+      System.out.println("Sum of these numbers: " + rif);
+   }
 }
